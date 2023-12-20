ae012694af4fba8bf7f90c92720dc504df3ce3a0
tanay-sgl <tanay.sagrolikar@gmail.com>
2023-12-20 17:49:27 +1100

    mutation

--- fitness.py
+++ fitness.py
@@ -4,10 +4,10 @@ from average_similar_movie_scores import average_similar_movie_scores
 
 
 
-def actual_fitness( prompt  , number_of_movies_to_compare_against):
+def fitness( prompt  , number_of_movies_to_compare_against):
     llm_response = llm(prompt)
     db_response = query_db(llm_response, number_of_movies_to_compare_against)
     return average_similar_movie_scores(db_response)
 
-def fitness_func(ga_instance, solution, solution_idx, prompt, number_of_movies_to_compare_against):
-    return actual_fitness(prompt, number_of_movies_to_compare_against)
+# def fitness_func(ga_instance, prompt, number_of_movies_to_compare_against):
+#     return actual_fitness(prompt, number_of_movies_to_compare_against)
---
+++ mutation.py
@@ -0,0 +1,18 @@
+from numpy.random import rand
+from wonderwords import RandomWord
+import random
+
+def mutation(prompt, mutation_rate):
+    prompt_array = prompt.split()
+
+    for index  in range(len(prompt_array)):
+        if rand() < mutation_rate:
+            prompt_array[index] = RandomWord().word()
+
+    if rand() < mutation_rate:
+        random.shuffle(prompt_array)
+
+    return ' '.join(prompt_array)
+
+print(mutation("Movie Batman, Omega Shadow", 0.2))
+
