5d92e61034972d026f169a3128779cb6f15ec5f0
Aldwin Arambulo <aara5073@uni.sydney.edu.au>
2023-12-20 18:03:27 +1100

    Implemented crossover function

---
+++ crossover.py
@@ -0,0 +1,14 @@
+import random
+
+def crossover(cls, p1, p2, r_cross):
+    # check for recombination
+    if random.random() < r_cross:
+        # select crossover point that is not on the end of the string
+        pt = random.randint(1, len(p1) - 2)
+        # perform crossover
+        c1 = p1[:pt] + p2[pt:]
+        c2 = p2[:pt] + p1[pt:]
+    else:
+        # children are slices of parents by default
+        c1, c2 = p1[:], p2[:]
+    return c1, c2
