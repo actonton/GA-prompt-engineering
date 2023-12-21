56da6f8a26215f40ac41dab4fa819ab0a98c7431
tanay-sgl <tanay.sagrolikar@gmail.com>
2023-12-21 17:43:41 +1100

    more_stuff

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -12,14 +12,20 @@ import random
 
 def genetic_algorithm(initial_words, number_of_prompts, number_of_prompts_to_select, number_of_movies_to_compare_against, mutation_rate, crossover_rate):
     #population = [generate_generation_0(initial_words, 10) for _ in range(number_of_prompts)]
+    print("Running Genetic Algorithm")
     population = generate_generation_0(initial_words, number_of_prompts)
-    selected_prompts = selection(population, number_of_prompts_to_select)
-    print(population)
+
     new_population = []
-    for i in range(0, len(selected_prompts), 2):
-        child1, child2 = crossover(selected_prompts[random.randint(0, len(selected_prompts) - 1)], selected_prompts[random.randint(0, len(selected_prompts) - 1)], crossover_rate)
-        new_population.append(mutation(child1, mutation_rate))
-        new_population.append(mutation(child2, mutation_rate))
+    selected_prompts = selection(population, number_of_prompts_to_select)
+    for _ in range(2):
+
+        for i in range(0, len(new_population), 2):
+            child1, child2 = crossover(selected_prompts[random.randint(0, len(selected_prompts) - 1)], selected_prompts[random.randint(0, len(selected_prompts) - 1)], crossover_rate)
+            new_population.append(mutation(child1, mutation_rate))
+            new_population.append(mutation(child2, mutation_rate))
+
+
+
 
     return new_population
 
@@ -33,4 +39,9 @@ mutation_rate = 0.1
 crossover_rate = 0.8
 
 result_population = genetic_algorithm(initial_words, number_of_prompts, number_of_prompts_to_select, number_of_movies_to_compare_against, mutation_rate, crossover_rate)
-print(result_population)
+#print(result_population)
+
+
+for result in result_population:
+    print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    print(result)
--- prompt_initial_generation.py
+++ prompt_initial_generation.py
@@ -18,25 +18,36 @@ def measure_difference(word_a,  word_b):
 # generating initial prompt
 # takes in data an array of words
 def generate_prompt(initial_words, target_output):
+    print("Generating Prompt")
     #target_scores = sentimentAnalyzer.polarity_scores(target_output)
     r = RandomWord()
     #additional_words = [r.word(include_parts_of_speech=["adjectives"]) for _ in range(len(initial_words))]
     additional_words = []
-    while(len(additional_words) < len(initial_words)):
-        potential_word = r.word()
-        word_from_initial_words_to_compare_against = initial_words[randint(0,len(initial_words) -1)]
-        if measure_difference(potential_word, word_from_initial_words_to_compare_against) > 0.5:
-            additional_words.append(potential_word)
     initial_words = initial_words.split()
+    count = 0
+    while(count < 10):
+        potential_word = r.word()
+        random_index = randint(0,len(initial_words) -1)
+        word_from_initial_words_to_compare_against = initial_words[random_index]
+        print("Comparing " + word_from_initial_words_to_compare_against +" " + potential_word)
+        measured_difference = measure_difference(potential_word, word_from_initial_words_to_compare_against)
+        print(measured_difference)
+        if measured_difference == 2.0:
+            #additional_words.append(potential_word)
+            initial_words[random_index] = potential_word
+            count += 1
+    #initial_words = initial_words.split()
     #additional_words = ' '.join(additional_words)
-    initial_prompt = initial_words + additional_words
+    initial_prompt = initial_words
     initial_prompt = ' '.join(initial_prompt)
     #initial_prompt = ' '.join(initial_words + additional_words)
    # best_result = best_prompt(target_output, initial_prompt, target_scores)
     return initial_prompt
 
 def generate_generation_0(initial_words, number_of_prompts):
-
+    print("Running Generation 0")
+    print(measure_difference("Happy", "Angry"))
+    #return [initial_words for _ in range(number_of_prompts + 10)]
     return [ generate_prompt(initial_words, "stuff") for i in range(number_of_prompts)]
 
 
